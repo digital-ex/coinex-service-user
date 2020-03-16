c74257730e6a1f09e6af30c43ca13971485efd2d
wangbinzero <wangbbhtt@gmail.com>
2020-03-16 13:29:12 +0800

    init

---
+++ src/main/java/com/exchange/coinex/CoinexServiceUserApplication.java
@@ -0,0 +1,13 @@
+package com.exchange.coinex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinexServiceUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinexServiceUserApplication.class, args);
+    }
+
+}
